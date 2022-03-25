c8472f9b09b1ec63ae5a451637c61933e0141dbe
Rodolfo Ghiggi <rodolfo@maxiprod.com.br>
2022-03-25 20:47:20 -0300

    Fix public

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-public static int Test()
+static int Test()
 {
     var a = 10 + 20;
     var b = a * 2;
     return b;
 }
 
-public static int TestA()
+static int TestA()
 {
     var c = 10 + 20;
     var d = c * 2;
     return d;
 }
 
-public static int TestB()
+static int TestB()
 {
     var a = 10 + 20;
     var b = a * 2;
