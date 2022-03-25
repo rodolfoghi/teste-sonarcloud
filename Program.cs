8126e61b6220d0fc9871cd4bc710651452426065
Rodolfo Ghiggi <rodolfo@maxiprod.com.br>
2022-03-25 20:44:39 -0300

    Commit 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public static int Test()
+{
+    var a = 10 + 20;
+    var b = a * 2;
+    return b;
+}
+
+public static int TestA()
+{
+    var c = 10 + 20;
+    var d = c * 2;
+    return d;
+}
+
+public static int TestB()
+{
+    var a = 10 + 20;
+    var b = a * 2;
+    return b;
+}
