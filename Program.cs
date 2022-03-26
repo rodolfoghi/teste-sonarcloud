232632432950397222315080d92c8c60e3b8ea9c
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2022-03-25 21:26:32 -0300

    Commit 1 (#1)
    * Commit 1
    
    * Fix public
    
    * Fix one code smell
    
    * Fix 2 code smell
    
    * Fix duplication

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static int Test()
+{
+    var a = 10 + 20;
+    var b = a * 2;
+    return b;
+}
+
+static int TestA()
+{
+    var c = 10 + 20;
+    var d = c * 2;
+    return d;
+}
+
+
+System.Console.WriteLine(Test());
+System.Console.WriteLine(TestA());
+System.Console.WriteLine(Test());
